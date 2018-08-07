e67c9f80b367407dc6aa3845117661a2e4cc079a
田村侑大 <yudai_lab@YudaiMacBookPro.local>
2018-08-07 20:19:11 +0900

    マイページの実行

--- wp-content/themes/twentyseventeen_child/functions.php
+++ wp-content/themes/twentyseventeen_child/functions.php
@@ -24,7 +24,7 @@ function remove_menu() {
 
 add_action('init', 'add_websites_post_type');
 function add_websites_post_type() {
-    $membersParams = array(
+    $pastDonationParams = array(
         'labels' => array(
             'name' => '過去の募金',
             'singular_name' => '過去の募金',
@@ -45,19 +45,125 @@ function add_websites_post_type() {
         "supports" => array( "title", "thumbnail" ),
         'menu_position' => 8,
     );
-    register_post_type('past-donation', $membersParams);
+	register_post_type('past-donation', $pastDonationParams);
+	
+	
+	$myPageParams = array(
+        'labels' => array(
+            'name' => 'マイページ',
+            'singular_name' => 'マイページ',
+            'add_new' => 'マイページを追加',
+            'add_new_item' => '新規マイページを追加',
+            'edit_item' => '編集',
+            'new_item' => '新規マイページ',
+            'all_items' => '全てのマイページ',
+            'view_item' => 'マイページを見る',
+            'search_items' => 'マイページを探す',
+            'not_found' => '見つかりませんでした',
+            'not_found_in_trash' => 'ゴミ箱の中にはありませんでした',
+            'enter_title_here' => 'マイページ名を入力',
+        ),
+        'public' => true,
+        'capability_type'    => 'post',
+        'has_archive' => true,
+        "supports" => array( "title", "thumbnail" ),
+        'menu_position' => 8,
+    );
+    register_post_type('mypage', $myPageParams);
 }
 
 
 if(function_exists("register_field_group")){
+	register_field_group(
+		array (
+			'id' => 'acf_%e9%81%8e%e5%8e%bb%e3%81%ae%e5%8b%9f%e9%87%91',
+			'title' => '過去の募金',
+			'fields' => array (
+				array (
+					'key' => 'field_5b6933680b1e3',
+					'label' => '募金された金額',
+					'name' => 'past-donation__donated-amount',
+					'type' => 'number',
+					'required' => 1,
+					'default_value' => '',
+					'placeholder' => '',
+					'prepend' => '',
+					'append' => '',
+					'min' => '',
+					'max' => '',
+					'step' => '',
+				),
+				array (
+					'key' => 'field_5b6938220b1e4',
+					'label' => '募金した人数',
+					'name' => 'past-donation__donated-people',
+					'type' => 'number',
+					'required' => 1,
+					'default_value' => '',
+					'placeholder' => '',
+					'prepend' => '',
+					'append' => '',
+					'min' => '',
+					'max' => '',
+					'step' => '',
+				),
+				array (
+					'key' => 'field_5b69383e0b1e5',
+					'label' => '実績',
+					'name' => 'past-donation__achievement',
+					'type' => 'textarea',
+					'required' => 1,
+					'default_value' => '',
+					'placeholder' => '',
+					'prepend' => '',
+					'append' => '',
+					'formatting' => 'html',
+					'maxlength' => '',
+				),
+				array (
+					'key' => 'field_5b6938d10b1e6',
+					'label' => '募金の募集してた期間',
+					'name' => 'past-donation__term',
+					'type' => 'text',
+					'required' => 1,
+					'default_value' => '',
+					'placeholder' => '',
+					'prepend' => '',
+					'append' => '',
+					'formatting' => 'html',
+					'maxlength' => '',
+				),
+			),
+			'location' => array (
+				array (
+					array (
+						'param' => 'post_type',
+						'operator' => '==',
+						'value' => 'past-donation',
+						'order_no' => 0,
+						'group_no' => 0,
+					),
+				),
+			),
+			'options' => array (
+				'position' => 'normal',
+				'layout' => 'no_box',
+				'hide_on_screen' => array (
+				),
+			),
+			'menu_order' => 0,
+		)	
+	);
+
+	
 	register_field_group(array (
-		'id' => 'acf_%e9%81%8e%e5%8e%bb%e3%81%ae%e5%8b%9f%e9%87%91',
-		'title' => '過去の募金',
+		'id' => 'acf_%e3%83%9e%e3%82%a4%e3%83%9a%e3%83%bc%e3%82%b8',
+		'title' => 'マイページ',
 		'fields' => array (
 			array (
-				'key' => 'field_5b6933680b1e3',
-				'label' => '募金された金額',
-				'name' => 'past-donation__donated-amount',
+				'key' => 'field_5b695605f6711',
+				'label' => '総募金額',
+				'name' => 'myPage__donation-amount',
 				'type' => 'number',
 				'required' => 1,
 				'default_value' => '',
@@ -67,46 +173,6 @@ if(function_exists("register_field_group")){
 				'min' => '',
 				'max' => '',
 				'step' => '',
-			),
-			array (
-				'key' => 'field_5b6938220b1e4',
-				'label' => '募金した人数',
-				'name' => 'past-donation__donated-people',
-				'type' => 'number',
-				'required' => 1,
-				'default_value' => '',
-				'placeholder' => '',
-				'prepend' => '',
-				'append' => '',
-				'min' => '',
-				'max' => '',
-				'step' => '',
-			),
-			array (
-				'key' => 'field_5b69383e0b1e5',
-				'label' => '実績',
-				'name' => 'past-donation__achievement',
-				'type' => 'textarea',
-				'required' => 1,
-				'default_value' => '',
-				'placeholder' => '',
-				'prepend' => '',
-				'append' => '',
-				'formatting' => 'html',
-				'maxlength' => '',
-			),
-			array (
-				'key' => 'field_5b6938d10b1e6',
-				'label' => '募金の募集してた期間',
-				'name' => 'past-donation__term',
-				'type' => 'text',
-				'required' => 1,
-				'default_value' => '',
-				'placeholder' => '',
-				'prepend' => '',
-				'append' => '',
-				'formatting' => 'html',
-				'maxlength' => '',
 			),
 		),
 		'location' => array (
@@ -114,7 +180,7 @@ if(function_exists("register_field_group")){
 				array (
 					'param' => 'post_type',
 					'operator' => '==',
-					'value' => 'past-donation',
+					'value' => 'mypage',
 					'order_no' => 0,
 					'group_no' => 0,
 				),
---
+++ wp-content/themes/twentyseventeen_child/single-mypage.php
@@ -0,0 +1,58 @@
+<?php
+get_header();
+
+$imageUrl = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'medium');
+$userName = get_the_title();
+$donatedAmount = get_field('myPage__donation-amount');
+
+?>
+
+
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <h1 class="main-visual">
+                My Page
+            </h1>
+
+            <div class="my-page__main">
+                <div class="my-page__user">
+                    <div class="flex-left">
+                        <div class="my-page__user__image-container">
+                            <img src="<?php echo $imageUrl; ?>">
+                        </div>
+                        <p class="my-page__user__name">
+                            <?php echo $userName; ?>
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    募金額： ¥ <?php echo $donatedAmount; ?>
+                </div>
+            </div>
+
+            <ul class="my-page__project">
+                <?php
+                $args = array('post_type' => 'past-donation');
+                $the_query = new WP_Query($args);
+                if($the_query->have_posts()){
+                    while($the_query->have_posts()){
+                        $the_query->the_post();
+
+                        $projectImageUrl = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'medium');
+                        $projectTitle = get_the_title();
+                        $projectDonatedAmount = get_field('past-donation__donated-amount');
+                ?>
+
+                <li class="my-page__project"></li>
+
+                <?php
+                    }
+                }
+                ?>
+            </ul>
+
+        </main>
+    </div>
+
+
+<?php get_footer();
