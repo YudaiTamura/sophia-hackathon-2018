260fd3dc3a690d349e66a11812dc444f38ed7598
田村侑大 <yudai_lab@YudaiMacBookPro.local>
2018-08-07 21:52:17 +0900

    header.phpの修正

--- wp-content/themes/twentyseventeen_child/header.php
+++ wp-content/themes/twentyseventeen_child/header.php
@@ -1,27 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-
-<meta charset="UTF-8" />
-<title>電子マネー時代における募金</title>
-<?php wp_head(); ?>
+    <meta charset="UTF-8"/>
+    <title>電子マネー時代における募金</title>
+    <?php wp_head(); ?>
 </head>
-  <body>
-  <div id = "headtab">
-      <div id = "menu">
-        <ul>
-          <li>
-            <a href = "http://localhost:8888/wordpress/">過去の募金活動</a>
-          </li>
-          <li>
-            <a href = "http://localhost:8888/wordpress/">現在の募金活動</a>
-          </li>
-          <li>
-            <a href = "http://localhost:8888/wordpress/">マイページ</a>
-          </li>
-          <!-- <a href = "" >過去の募金活動</a>
-          <a href = "">現在の募金活動</a>
-          <a href = "">MyPage</a> -->
+
+<?php
+$args = array('post_type' => 'mypage');
+$the_query = new WP_Query($args);
+$singleMyPageID = "";
+if($the_query->have_posts()){
+    $singleMyPageID = get_the_ID();
+}
+?>
+
+<body>
+<div class="headtab">
+    <div class="menu">
+        <ul class="menu-list">
+            <li class="menu-list__item">
+                <a href="<?php get_post_type_archive_link('past-donation'); ?>">過去の募金活動</a>
+            </li>
+            <li class="menu-list__item">
+                <a href="<?php echo home_url();?>">現在の募金活動</a>
+            </li>
+            <li class="menu-list__item">
+                <a href="<?php get_post_permalink($singleMyPageID) ?>">マイページ</a>
+            </li>
         </ul>
-      </div>
-  </div>
+    </div>
+</div>
