1bdda0027d8423d905ca9026f45005de13924ae8
田村侑大 <yudai_lab@YudaiMacBookPro.local>
2018-08-08 14:05:09 +0900

    過去の募金一覧のhtml構造を変更

--- wp-content/themes/twentyseventeen_child/archive-past-donation.php
+++ wp-content/themes/twentyseventeen_child/archive-past-donation.php
@@ -4,31 +4,27 @@ get_header();
 
     <div class="content-area">
         <main class="site-main archive-project">
-            <h1 class="main-visual">過去の募金一覧</h1>
+            <h1 class="main-visual">過去のプロジェクト</h1>
             <ul class="project__article-container">
                 <?php
                 if (have_posts()) {
                     while (have_posts()) {
                         the_post();
 
-                        $imageUrl = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'medium');
+                        $imageUrl = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'medium')[0];
                         $title = get_the_title();
                         $donated_amount = get_field('past-donation__donated-amount');
-                        $donated_people = get_field('past-donation__donated-people');
-                        $term = get_field('past-donation__term');
-                        $achievement = get_field('past-donation__achievement');
 
                         ?>
                         <li class="project__article-container__article">
-                            <p class="project__article-container__article__title"><?php echo $title ?></p>
-                            <p class="project__article-container__article__donated-amount">合計金額: <?php echo $donated_amount ?>円</p>
-                            <p class="project__article-container__article__donated-people">参加人数: <?php echo $donated_people ?>人</p>
-                            <p class="project__article-container__article__term">募集期間: <?php echo $term ?></p>
-                            <div class="project__article-container__article__achievement">
-                                <p class="project__article-container__article__achievement__title">実績</p>
-                                <div class="project__article-container__article__achievement__content">
-                                    <?php echo $achievement ?>
-                                </div>
+                            <div class="project__article-container__article__image-container">
+                                <div class="project__article-container__article__image-container__content"
+                                     data-src="<?php echo $imageUrl; ?>"
+                                     style="background-image: url(<?php echo $imageUrl; ?>);"></div>
+                            </div>
+                            <div class="project__article-container__article__title-amount">
+                                <p class="project__article-container__article__title-amount__title"><?php echo $title ?></p>
+                                <p class="project__article-container__article__title-amount__donated-amount"><?php echo $donated_amount ?>円</p>
                             </div>
                         </li>
                         <?php
